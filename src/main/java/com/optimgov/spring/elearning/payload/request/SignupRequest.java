d6c11d6fec1c0d5d1304575e93ba9ffe8eabf40c
chakibma592 <maoura.chakib@uit.ac.ma>
2021-06-11 22:09:09 +0100

    11juin_2

--- src/main/java/com/optimgov/spring/elearning/payload/request/SignupRequest.java
+++ src/main/java/com/optimgov/spring/elearning/payload/request/SignupRequest.java
@@ -19,8 +19,7 @@ public class SignupRequest {
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
-    @NotBlank
-    @Size(min = 6, max = 40)
+   
     private String newpassword;
     public String getUsername() {
         return username;
