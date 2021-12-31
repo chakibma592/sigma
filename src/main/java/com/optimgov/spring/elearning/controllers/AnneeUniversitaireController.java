c2c22bc465272928b7f52cbec4eaeacd973acf70
chakibma592 <maoura.chakib@uit.ac.ma>
2021-12-31 17:06:14 +0100

    lastone

---
+++ src/main/java/com/optimgov/spring/elearning/controllers/AnneeUniversitaireController.java
@@ -0,0 +1,91 @@
+package com.optimgov.spring.elearning.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.optimgov.spring.elearning.models.AnneeUniversitaire;
+import com.optimgov.spring.elearning.models.Semestre;
+import com.optimgov.spring.elearning.payload.response.MessageResponse;
+import com.optimgov.spring.elearning.repository.AnneeUniversitaireRepository;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/annees")
+public class AnneeUniversitaireController {
+	@Autowired
+    private AnneeUniversitaireRepository anneeRepository;
+	
+	@GetMapping("/list/{id}")
+	public ResponseEntity<AnneeUniversitaire>getAnnee(@PathVariable("id") String id) {
+		 try {
+			 
+
+		   
+
+		      return new ResponseEntity<>(anneeRepository.findByAnneeId(Long.parseLong(id)), HttpStatus.OK);
+		    } catch (Exception e) {
+		      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		    }
+	  }
+	@GetMapping("/list")
+	public ResponseEntity<ArrayList<AnneeUniversitaire>>getAnneeList() {
+		 try {
+			 ArrayList<AnneeUniversitaire> annees = new ArrayList<AnneeUniversitaire>();
+			 anneeRepository.findAll().forEach(annees::add);
+			 if (annees.isEmpty()) {
+			        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			      }
+
+			      return new ResponseEntity<>(annees, HttpStatus.OK);
+		    } catch (Exception e) {
+		      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		    }
+	  }
+	@PostMapping("/add")
+	public ResponseEntity<?>  createAnnee(@RequestBody AnneeUniversitaire anneerequest) {
+		try {
+			AnneeUniversitaire annee = anneeRepository
+					.save(new AnneeUniversitaire(anneerequest.getAnneename()));
+			return ResponseEntity.ok(new MessageResponse("Year added succesfully!"));
+		} catch (Exception e) {
+			return ResponseEntity.ok(new MessageResponse("Year not added!"));
+		}
+	}
+	@PutMapping("/update/{id}")
+	public ResponseEntity<AnneeUniversitaire> updateAnnee(@PathVariable("id") long id,@RequestBody AnneeUniversitaire anneerequest) {
+		Optional<AnneeUniversitaire> anneeData = anneeRepository.findById(id);
+
+		if (anneeData.isPresent()) {
+			AnneeUniversitaire annee = anneeData.get();
+			annee.setAnneename(anneerequest.getAnneename());
+			
+			
+			
+			return new ResponseEntity<>(anneeRepository.save(annee), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteannee(@PathVariable("id") long id) {
+		try {
+			anneeRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
---
+++ src/main/java/com/optimgov/spring/elearning/models/AnneeUniversitaire.java
@@ -0,0 +1,53 @@
+package com.optimgov.spring.elearning.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(	name = "annees")
+public class AnneeUniversitaire implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotBlank
+	@Size(max = 200)
+	private String anneename;
+	public AnneeUniversitaire(Long id, @NotBlank @Size(max = 200) String anneename) {
+		super();
+		this.id = id;
+		this.anneename = anneename;
+	}
+	public AnneeUniversitaire(@NotBlank @Size(max = 200) String anneename) {
+		super();
+		this.anneename = anneename;
+	}
+	public AnneeUniversitaire() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAnneename() {
+		return anneename;
+	}
+	public void setAnneename(String anneename) {
+		this.anneename = anneename;
+	}
+	
+
+}
