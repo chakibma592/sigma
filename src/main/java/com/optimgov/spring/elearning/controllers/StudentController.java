cbbbbba6c01d53b44c4fc18945c662a9d299fe11
chakibma592 <maoura.chakib@uit.ac.ma>
2022-01-08 00:34:31 +0100

    comm08012022

--- src/main/java/com/optimgov/spring/elearning/controllers/StudentController.java
+++ src/main/java/com/optimgov/spring/elearning/controllers/StudentController.java
@@ -12,7 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +38,7 @@ import com.optimgov.spring.elearning.security.jwt.JwtUtils;
 public class StudentController {
 	@Autowired
     private StudentRepository studentRepository;
+	
 	@Autowired
 	AuthenticationManager authenticationManager;
 
@@ -97,5 +100,14 @@ public class StudentController {
 		student.setFiliere(filiere);
 		studentRepository.save(student);		
   		return ResponseEntity.ok(new MessageResponse("Student registered successfully!"));
+	}
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") long id) {
+		try {
+			userRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
