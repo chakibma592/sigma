4842d0214a2b50387c535f35f00e0c26daf4217e
chakibma592 <maoura.chakib@uit.ac.ma>
2022-01-26 12:12:57 +0100

    commitlyoum

--- src/main/java/com/optimgov/spring/elearning/controllers/NoteController.java
+++ src/main/java/com/optimgov/spring/elearning/controllers/NoteController.java
@@ -1,10 +1,14 @@
 package com.optimgov.spring.elearning.controllers;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.optimgov.spring.elearning.models.AnneeUniversitaire;
 import com.optimgov.spring.elearning.models.ElementModule;
-import com.optimgov.spring.elearning.models.Module;
+import com.optimgov.spring.elearning.models.Inscription;
 import com.optimgov.spring.elearning.models.Note;
 import com.optimgov.spring.elearning.models.Semestre;
 import com.optimgov.spring.elearning.models.Student;
-import com.optimgov.spring.elearning.models.Teacher;
-import com.optimgov.spring.elearning.payload.request.ElementRequest;
 import com.optimgov.spring.elearning.payload.request.NoteRequest;
 import com.optimgov.spring.elearning.payload.response.MessageResponse;
 import com.optimgov.spring.elearning.repository.AnneeUniversitaireRepository;
 import com.optimgov.spring.elearning.repository.ElementRepositoy;
+import com.optimgov.spring.elearning.repository.InscriptionRepository;
 import com.optimgov.spring.elearning.repository.NoteRepository;
 import com.optimgov.spring.elearning.repository.SemestreRepository;
 import com.optimgov.spring.elearning.repository.StudentRepository;
@@ -33,6 +36,8 @@ import com.optimgov.spring.elearning.repository.StudentRepository;
 public class NoteController {
 	@Autowired
     private ElementRepositoy elementRepository;
+	@Autowired
+    private InscriptionRepository  inscriptionRepository;
 	@Autowired
     private StudentRepository studentRepository;
 	@Autowired
@@ -67,4 +72,23 @@ public class NoteController {
 			return ResponseEntity.ok(new MessageResponse(text));
 		}
 	}
+	@GetMapping("/bulletin/{id}")
+	public ResponseEntity<ArrayList<Note>>getNotesByStudent(@PathVariable("id") String id) {
+		 try {
+			 ArrayList<Note> notes = new ArrayList<Note>();
+			 Inscription inscription= new Inscription();
+			 if (id != null)
+				 inscription=inscriptionRepository.findInscriptionEnCoursByStudent(Long.parseLong(id));
+		     
+		      noteRepository.findByStudentId(Long.parseLong(id),inscription.getAnnee(),inscription.getSemestre()).forEach(notes::add);
+		      
+		      if (notes.isEmpty()) {
+		        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		      }
+
+		      return new ResponseEntity<>(notes, HttpStatus.OK);
+		    } catch (Exception e) {
+		      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		    }
+	  }
 }
--- src/main/java/com/optimgov/spring/elearning/repository/NoteRepository.java
+++ src/main/java/com/optimgov/spring/elearning/repository/NoteRepository.java
@@ -1,14 +1,21 @@
 package com.optimgov.spring.elearning.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.optimgov.spring.elearning.models.AnneeUniversitaire;
 import com.optimgov.spring.elearning.models.Note;
+import com.optimgov.spring.elearning.models.Semestre;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long>{
 	@Query("SELECT c FROM Note c where c.id = :identity")
 	Note findByNoteId(@Param(value="identity")Long id);
+	@Query("SELECT c FROM Note c where c.studentid = :identity AND c.annee= :annee AND c.semestre= :semestre")
+	ArrayList<Note>findByStudentId(@Param(value="identity")Long id, @Param(value="annee")AnneeUniversitaire annee, @Param(value="semestre")Semestre semestre);
 
 }
