d528aef377a7f65da60c91eb4388faa5d9b5db28
chakibma592 <maoura.chakib@uit.ac.ma>
2022-02-06 01:37:39 +0100

    commitnuit2

--- src/main/java/com/optimgov/spring/elearning/controllers/NoteController.java
+++ src/main/java/com/optimgov/spring/elearning/controllers/NoteController.java
@@ -148,11 +148,16 @@ public class NoteController {
 		    	  nbrelement=0;
 		    	  notemodule=0;
 		    	  listelement= new ArrayList<ElementResponse>();
-		    	  }
 		    	  nbrelement++;
 		    	  notemodule+=n.getNote();
 		    	  ElementResponse elemementrep=new ElementResponse(n.getElement().getElementname(), n.getNote());
 		    	  listelement.add(elemementrep);
+		    	  }else {
+		    	  nbrelement++;
+		    	  notemodule+=n.getNote();
+		    	  ElementResponse elemementrep=new ElementResponse(n.getElement().getElementname(), n.getNote());
+		    	  listelement.add(elemementrep);
+		    	  }
 		      }
 		      return new ResponseEntity<>(moyenne, HttpStatus.OK);
 		    } catch (Exception e) {
